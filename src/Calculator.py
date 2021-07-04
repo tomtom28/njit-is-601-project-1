3d266c7ffec58d435a3739a80f99b3ed403de015
Thomas Thompson <tomthompson828@gmail.com>
2021-07-04 19:04:33 -0400

    Feature: Added Addition Test and Methods

--- src/Calculator.py
+++ src/Calculator.py
@@ -1,3 +1,17 @@
 class Calculator:
+
+    # Class Methods:
+    @classmethod
+    def addition(cls, a, b):
+        return int(a) + int(b)
+
+    # Initialization:
+    result = 0
+
     def __init__(self):
         pass
+
+    # Object Methods:
+    def add(self, a, b):
+        self.result = self.addition(a, b)
+        return self.result
--- src/TestCalculator.py
+++ src/TestCalculator.py
@@ -1,12 +1,24 @@
 import unittest
+import TestProperties
 
 from Calculator import Calculator
+from CsvReader import CsvReader
+
 
 
 class MyTestCase(unittest.TestCase):
-    def test_instantiate_calulator(self):
-        calculator = Calculator()
-        self.assertIsInstance(calculator, Calculator)
+
+    def setUp(self) -> None:
+        self.calculator = Calculator()
+
+    def test_calculator_instantiate(self):
+        self.assertIsInstance(self.calculator, Calculator)
+
+    def test_calculator_addition(self):
+        test_data = CsvReader(TestProperties.ADDITION_FILE_NAME).data
+        for row in test_data:
+            self.assertEqual(self.calculator.add(row['Value 1'], row['Value 2']), int(row['Result']))
+            self.assertEqual(self.calculator.result, int(row['Result']))
 
 
 if __name__ == '__main__':
